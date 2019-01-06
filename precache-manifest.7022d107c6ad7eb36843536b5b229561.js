0901c6d4eda1abde39184c4733ed37b21f59f2b5
Kenneth Reilly <kenneth.reilly@hotmail.com>
2019-01-06 09:26:35 -0800

    Updates

---
+++ precache-manifest.7022d107c6ad7eb36843536b5b229561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9da2c3947e8fa45dbe37",
+    "url": "/trak/static/css/main.9e5d2d8c.chunk.css"
+  },
+  {
+    "revision": "9da2c3947e8fa45dbe37",
+    "url": "/trak/static/js/main.9da2c394.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/trak/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f6e83dea0497f59ca9bc",
+    "url": "/trak/static/js/runtime~main.f6e83dea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trak/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "737dcde8a2943402b70e4e0dff32324f",
+    "url": "/trak/index.html"
+  }
+];
