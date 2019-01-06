cf2067dc1ddb2ce9484ef0e5bf010e87e32a9b77
Kenneth Reilly <kenneth.reilly@hotmail.com>
2019-01-06 09:13:50 -0800

    Testing React app deployment to gh-pages /docs

---
+++ docs/precache-manifest.0f49f5a83aecbd113784179d8172fa48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "caa7c4bc414e6e100b3f",
+    "url": "/static/css/main.9e5d2d8c.chunk.css"
+  },
+  {
+    "revision": "caa7c4bc414e6e100b3f",
+    "url": "/static/js/main.caa7c4bc.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc3fc3c47a1eea3795858cef74d4f23f",
+    "url": "/index.html"
+  }
+];
